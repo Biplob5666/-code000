4241c76c1882cd59468090044fe94f4b8bbd296a
Biplob5666 <mr01830004056@gmail.com>
2025-10-08 01:29:47 +0600

    Create post-create.sh

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Running post-create setup..."
+
+# Optional: Install additional Python tools
+pip install --upgrade pip
+pip install requests flask
+
+echo "Setup complete! Ready to hack 😎"
